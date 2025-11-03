7c7a4480c30f0b4957acdb9ec5f99d8af4085376
Gadigeppa sorati <41178974+gsorati@users.noreply.github.com>
2025-11-03 21:59:15 +0530

    Adding custom middleware

---
+++ CoreApplication/CoreApplication/CustomMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CoreApplication
+{
+    public class CustomMiddleware
+    {
+        public readonly RequestDelegate _next;
+
+        public CustomMiddleware(RequestDelegate deligate) => _next = deligate;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Console.WriteLine("Custom Middleware Executing Before Next");
+            await this._next(context);
+        }
+    }
+}
